d0e19dddafe79dd3416fe8110ec21dc7a67feced
GubbinLubbin <silwalankit27@gmail.com>
2026-03-21 11:09:50 +0530

    Parsed:Parsed the pooler values and implemented the shared types

--- apps/engine/src/consumer.ts
+++ apps/engine/src/consumer.ts
@@ -1,5 +1,6 @@
+import { parseFields } from "./parser";
 import { redis } from "./redis";
-
+import type { PriceEvent } from "@option_trading/shared";
 export async function startConsumer(){
   console.log("Engine listening to the stream sir..")
 
@@ -14,11 +15,20 @@ export async function startConsumer(){
 
     if(!response) continue;
 
-    const [stream,messages]=response[0];
+    const [,messages]=response[0];
 
     for(const message of messages){
-      const [id,fields]=message;
-      console.log("Event received",fields)
+      const [,fields]=message;
+      const parsed=parseFields(fields);
+      if(parsed.type==="PRICE_UPDATE"){
+        const event:PriceEvent={
+          type:"PRICE_UPDATE",
+          symbol:parsed.symbol,
+          price:Number.parseFloat(parsed.price),
+          timestamp:Number.parseInt(parsed.timestamp)
+        };
+        console.log("Parsed Event",event);
+      }
     }
 
   }
